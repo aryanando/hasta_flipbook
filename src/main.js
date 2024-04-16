3151937e4411f0faeb0aaa23095bd326af05671c
Aryanando Mac <aryanando232@gmail.com>
2024-04-16 07:30:03 +0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Vue.use(BootstrapVue)
+
+createApp(App).mount('#app')
